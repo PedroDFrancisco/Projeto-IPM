98e421454f84f9937e3e4ebfbae25aa6596ab41d
GoncaloVicencio <91968112+GoncaloVicencio@users.noreply.github.com>
2022-11-20 16:51:48 +0000

    images

---
+++ exploringPortugal/js/feed.js
@@ -0,0 +1,10 @@
+function suggestions1(){
+    var sp = document.getElementById("sp1");
+    const img = sp.querySelector("img");
+    var name = document.createElement("h2");
+    var followers = document.createElement("h3");
+    
+    
+    name.innerHTML = "RicFazeres";
+    followers.innerHTML = "400k followers"
+}
