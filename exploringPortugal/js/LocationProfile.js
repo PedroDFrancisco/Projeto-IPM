f83dde2978bd07b3857f6a7cf115d2c85401dde4
PedroDFrancisco <p.francisco@campus.fct.unl.pt>
2022-11-23 16:23:20 +0000

    locationProfile

--- exploringPortugal/js/LocationProfile.js
+++ exploringPortugal/js/LocationProfile.js
@@ -16,32 +16,61 @@ function LocProfileLoad() {
         let phonehtml = document.getElementById('phone');
         phonehtml.innerHTML = phone.replaceAll("-", " ");
         let websitehtml = document.getElementById('website');
-        websitehtml.innerHTML = website;
+        websitehtml.innerHTML = "Website";//website;
+        websitehtml.href = website;
 	}
 
   let submitCom = document.getElementById('submmit-comment');
   let divCom = document.getElementById('add-comment');
 
-    submitCom.addEventListener("click", function () {
-        let comment = document.getElementById('comment-text');
+  submitCom.addEventListener("click", function () {
+      let comment = document.getElementById('comment-text');
 
-        if (comment.value != "") {
-            let row = document.createElement('div');
-            row.className = "realComment"
-            row.innerHTML = "<br>"
+      if (comment.value != "") {
+          let row = document.createElement('div');
+          row.className = "realComment"
+          row.innerHTML = "<br>"
 
-            let img = document.createElement('img');
-            img.className = "comment-image";
-            img.src = "../../images/nova logo" + ".png";
+          let img = document.createElement('img');
+          img.className = "comment-image";
+          img.src = "../../images/nova logo" + ".png";
 
-            let text = document.createElement("p");
-            text.className = "comment-text";
-            text.innerHTML = "<b>NOVA Student</b>" + "<br>" + comment.value;
+          let text = document.createElement("p");
+          text.className = "comment-text";
+          text.innerHTML = "<b>NOVA Student</b>" + "<br>" + comment.value;
 
-            row.appendChild(img);
-            row.appendChild(text);
-            divCom.appendChild(row);
-            comment.value = '';
-        }
-    });
+          row.appendChild(img);
+          row.appendChild(text);
+          divCom.appendChild(row);
+          comment.value = '';
+      }
+  });
+
+  let weather = document.getElementById('weather');
+  let counter = 0;
+
+  weather.addEventListener("click", function () {
+      if(counter == 0) {
+        weather.src = "../../images/weatherFriday.png";
+      }
+      if(counter == 1) {
+        weather.src = "../../images/weatherSaturday.png";
+      }
+      counter++;
+  });
+
+  let follow = document.getElementById('follow');
+  let binary = 0;
+
+  follow.addEventListener("click", function () {
+    if(binary == 0) {
+      follow.classList.add("followed");
+      follow.innerHTML = "Following"
+      binary++;
+    } else {
+      follow.classList.remove("followed");
+      follow.innerHTML = "Follow"
+      binary--;
+    }
+  });
 }
