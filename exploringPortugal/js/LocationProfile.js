8142cf13a0273ea9fafa8f66aef870f6cb3d54c0
PedroDFrancisco <p.francisco@campus.fct.unl.pt>
2022-11-23 15:38:42 +0000

    new

--- exploringPortugal/js/LocationProfile.js
+++ exploringPortugal/js/LocationProfile.js
@@ -1,23 +1,47 @@
 function LocProfileLoad() {
     let urlParams = new URLSearchParams(window.location.search);
-  	let name = urlParams.get('name').replaceAll("-", " ");  
-    let location = urlParams.get('location').replaceAll("-", " ");
-    let schedule = urlParams.get('schedule').replaceAll("_", " ");  
-    let phone = urlParams.get('phone').replaceAll("-", " ");  
+  	let name = urlParams.get('name');  
+    let location = urlParams.get('location');
+    let schedule = urlParams.get('schedule');  
+    let phone = urlParams.get('phone');  
     let website = urlParams.get('website');    
 
 	if(name != null || location != null || schedule != null || phone != null || website != null) {
 		let namehtml = document.getElementById('name');
-        namehtml.innerHTML = name;
+        namehtml.innerHTML = name.replaceAll("-", " ");
         let locationhtml = document.getElementById('location');
-        locationhtml.innerHTML = location;
+        locationhtml.innerHTML = location.replaceAll("-", " ");
         let schedulehtml = document.getElementById('schedule');
-        schedulehtml.innerHTML = schedule;
+        schedulehtml.innerHTML = schedule.replaceAll("_", " ");
         let phonehtml = document.getElementById('phone');
-        phonehtml.innerHTML = phone;
+        phonehtml.innerHTML = phone.replaceAll("-", " ");
         let websitehtml = document.getElementById('website');
         websitehtml.innerHTML = website;
-	} else {
-		map = new Map(MAP_CENTRE, 5);
 	}
+
+  let submitCom = document.getElementById('submmit-comment');
+  let divCom = document.getElementById('add-comment');
+
+    submitCom.addEventListener("click", function () {
+        let comment = document.getElementById('comment-text');
+
+        if (comment.value != "") {
+            let row = document.createElement('div');
+            row.className = "realComment"
+            row.innerHTML = "<br>"
+
+            let img = document.createElement('img');
+            img.className = "comment-image";
+            img.src = "../../images/nova logo" + ".png";
+
+            let text = document.createElement("p");
+            text.className = "comment-text";
+            text.innerHTML = "<b>NOVA Student</b>" + "<br>" + comment.value;
+
+            row.appendChild(img);
+            row.appendChild(text);
+            divCom.appendChild(row);
+            comment.value = '';
+        }
+    });
 }
